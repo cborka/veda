63cacce58b83266c1b2ff87314f781f089f6ee4c
Boris Ts. (cborka) <cborka777@yandex.ru>
2023-04-21 15:12:32 +0700

    230421 icons

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,11 +8,38 @@
 
 <Menu></Menu>
 
-<span class = "icon">
+<!-- <span class = "icon is-small has-text-info">
   <i class = "fas fa-user"></i>
+</span> -->
+<!-- <span class = "icon">
+  <i class="fa-sharp fa-light fa-truck-fast"></i>
+</span> -->
+
+<ion-icon name="accessibility-outline"></ion-icon>
+<ion-icon name="dice"></ion-icon>
+
+<span class = "icon  is-large">
+  <ion-icon name="accessibility-outline"></ion-icon>
+  <ion-icon name="dice"></ion-icon>
 </span>
 
+<ion-icon src="/alarm.svg"></ion-icon>
+<ion-icon src="/violin.svg" style="color: red"></ion-icon> 
 
+<!-- <div class="mdi-set is-large">
+  <span class="mdi-star"></span>
+  <span class="mdi-star"></span>
+  <span class="mdi-piano has-text-info"></span>
+  <span class="mdi-star-outline is-small"></span>
+  <span class="mdi-star-outline"></span>
+</div> -->
+<span class = "icon is-large">
+  <img src="/alarm.svg" alt="zz" />
+</span>
+<span class = "icon" >
+  <img src="/favicon.png" alt="zz" />
+<span class="has-text-danger">&#9773;</span>
+</span>
 <nav>
 	<a href="/">Home</a>
 	<a href="/about">About</a>
@@ -30,4 +57,7 @@
     color: green;
     font-weight: bold;
   } */
+  ion-icon {
+    /* color: red; */
+  }
 </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,29 +1,8 @@
 <h3 class="title is-3">О проекте</h3>
 
-
-<form class="box">
-  <div class="field">
-    <label class="label">Email</label>
-    <div class="control">
-      <input class="input" type="email" placeholder="e.g. alex@example.com">
-    </div>
-  </div>
-
-  <div class="field">
-    <label class="label">Password</label>
-    <div class="control">
-      <input class="input" type="password" placeholder="********">
-    </div>
-  </div>
-
-  <button class="button is-primary">Sign in</button>
-</form>
-
-
-
 <style>
-  /* h1 {
+  h3 {
     color: darkviolet;
-    font-weight: bold;
-  } */
+    /* font-weight: bold; */
+  }
 </style>
