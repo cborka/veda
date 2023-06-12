e6fa175316ebbccc4c7d65dc51384b8be4ccf291
Boris Ts <cborka777@yandex.ru>
2023-06-12 11:52:55 +0700

    230611 getClientIp()

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,13 +2,8 @@
 export async function handle({ event, resolve }) {
 
   console.log('Hook');
-  // console.log('Hook '+JSON.stringify(event.request.socket.remoteAddress));
-  // console.log(event.request.socket.remoteAddress);
-  console.log(event.request[Symbol.state]);
-  //console.log(JSON.stringify(event.request[Symbol.state]));
-
-
-
+  console.log('event.clientAddress='+event.getClientAddress()); // !!!
+  
   if (event.url.pathname.startsWith('/custom')) {
       return new Response('custom response');
   }
